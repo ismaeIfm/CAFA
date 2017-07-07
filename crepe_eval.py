e136df87d4115ec30e0c06534c9313987b8329a6
Ismael <ismaelfm_@ciencias.unam.mx>
2017-07-07 16:31:29 -0500

    Adding crepe eval

---
+++ crepe_eval.py
@@ -0,0 +1,77 @@
+import logging
+from random import shuffle
+
+import tensorflow as tf
+from keras.callbacks import EarlyStopping
+
+from mlom.datasets.create_CAFATrainingData import create_go_cafa_dataset
+from mlom.encoders.input.text import CrepeEncoder
+from mlom.encoders.output import OutputEncoder
+from mlom.mlom import MLOM
+from mlom.models import get_crepe_model
+from mlom.utils import AUCCallback, get_data
+
+if __name__ == '__main__':
+
+    logging.info('*** Loading data...')
+    data = create_go_cafa_dataset(cafa_version=2) + create_go_cafa_dataset(
+        cafa_version=3)
+    shuffle(data)
+
+    X_name = 'seq'
+    y_names = ['C', 'F']
+
+    logging.info('*** Formatting data...')
+    data = get_data(data=data, X_name=X_name, y_names=y_names)
+
+    valid_dataset = data
+    label_encoders = {}
+    for l in y_names:
+        label_encoders[l] = OutputEncoder()
+        label_encoders[l].fit(data[l])
+
+    ############################################################################
+    crepe_enc = CrepeEncoder(maxlen=150)
+    crepe_model = get_crepe_model(input_encoder=crepe_enc)
+
+    crepe = {'model': crepe_model, 'input_encoder': crepe_enc}
+    ############################################################################
+
+    m = MLOM(
+        name='crepe_vae',
+        models=[crepe],
+        format_={
+            'X':
+            X_name,
+            'y': [{
+                'name': label,
+                'activation': 'sigmoid',
+                'encoder': label_encoders[label],
+                'vae': True
+            } for label in y_names]
+        },
+        verbose=1)
+
+    m.compile(optimizer='adamax', loss='binary_crossentropy')
+
+    callbacks = [
+        EarlyStopping(monitor='val_loss', patience=10),
+        AUCCallback(
+            dirname='roc/cafa/%s' % m.name,
+            data=valid_dataset,
+            X_name=X_name,
+            y_names=y_names,
+            save=False,
+            label_encoders=label_encoders,
+            input_encoders=m.get_input_encoders())
+    ]
+
+    m.fit_generator(
+        data=data,
+        encoded=False,
+        validation_data=valid_dataset,
+        batch_size=128,
+        callbacks=callbacks,
+        epochs=100000,
+        #pretrain_epochs=1000,
+        verbose=1)
